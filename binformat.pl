64ad52e64a0b02b6e30ee4e684c79fa309dd7de8
Danielle Church <dani.church@gmail.com>
2021-08-05 15:29:35 -0400

    ADDED: initial low-level binformat predicates

--- binformat.pl
+++ binformat.pl
@@ -1,1 +1,148 @@
-:- library(binformat, []).
+:- module(binformat, [% Low-level DCG nonterminals
+                      bin_uint8//1,
+                      bin_sint8//1,
+                      bin_uint16//1,
+                      bin_sint16//1,
+                      bin_uint32//1,
+                      bin_sint32//1,
+                      bin_uint64//1,
+                      bin_sint64//1,
+                      % Endian-specific DCG nonterminals
+                      bin_uint16_le//1,
+                      bin_sint16_le//1,
+                      bin_uint32_le//1,
+                      bin_sint32_le//1,
+                      bin_uint64_le//1,
+                      bin_sint64_le//1,
+                      bin_uint16_be//1,
+                      bin_sint16_be//1,
+                      bin_uint32_be//1,
+                      bin_sint32_be//1,
+                      bin_uint64_be//1,
+                      bin_sint64_be//1,
+
+                      % Signed/unsigned conversion
+                      bin_conv_u8_s8/2,
+                      bin_conv_u16_s16/2,
+                      bin_conv_u32_s32/2,
+                      bin_conv_u64_s64/2,
+
+                      % Endianness utilities
+                      bin_swap16/2,
+                      bin_swap32/2,
+                      bin_swap64/2,
+                      bin_conv_u16_le/2,
+                      bin_conv_u32_le/2,
+                      bin_conv_u64_le/2,
+                      bin_conv_u16_be/2,
+                      bin_conv_u32_be/2,
+                      bin_conv_u64_be/2,
+                      bin_conv_s16_le/2,
+                      bin_conv_s32_le/2,
+                      bin_conv_s64_le/2,
+                      bin_conv_s16_be/2,
+                      bin_conv_s32_be/2,
+                      bin_conv_s64_be/2,
+                      htons/2,
+                      htonl/2,
+                      ntohs/2,
+                      ntohl/2]).
+:- use_foreign_library(foreign(binformat4pl)).
+
+%!  bin_uint8(?Int)// is semidet.
+%!  bin_sint8(?Int)// is semidet.
+%!  bin_uint16(?Int)// is semidet.
+%!  bin_sint16(?Int)// is semidet.
+%!  bin_uint32(?Int)// is semidet.
+%!  bin_sint32(?Int)// is semidet.
+%!  bin_uint64(?Int)// is semidet.
+%!  bin_sint64(?Int)// is semidet.
+%
+%   DCG nonterminals that convert between Prolog integers and their binary
+%   representations. These predicates all use *native* byte ordering; for
+%   explicit orderings (of multibyte integers) append `_be` or `_le` to the
+%   predicate name, for example bin_uint16_le//1 or bin_sint64_be//1.
+%
+%   These predicates (along with their endian variants) will silently truncate
+%   out-of-range integers when operating in (+)// mode.
+%
+%   @param Int is a Prolog integer.
+
+
+%!  bin_uint16_le(?Int)// is semidet.
+%!  bin_sint16_le(?Int)// is semidet.
+%!  bin_uint32_le(?Int)// is semidet.
+%!  bin_sint32_le(?Int)// is semidet.
+%!  bin_uint64_le(?Int)// is semidet.
+%!  bin_sint64_le(?Int)// is semidet.
+%!  bin_uint16_be(?Int)// is semidet.
+%!  bin_sint16_be(?Int)// is semidet.
+%!  bin_uint32_be(?Int)// is semidet.
+%!  bin_sint32_be(?Int)// is semidet.
+%!  bin_uint64_be(?Int)// is semidet.
+%!  bin_sint64_be(?Int)// is semidet.
+%
+%   DCG nonterminals that convert between Prolog integers and binary
+%   representations with explicit byte ordering, rather than host-native
+%   byte ordering. See e.g. bin_uint16//1 for more details.
+
+
+%!  bin_conv_u8_s8(?UInt, ?SInt) is det.
+%!  bin_conv_u16_s16(?UInt, ?SInt) is det.
+%!  bin_conv_u32_s32(?UInt, ?SInt) is det.
+%!  bin_conv_u64_s64(?UInt, ?SInt) is det.
+%
+%   These predicates are true when UInt is a non-negative integer that can be
+%   represented in the given number of bits, and SInt is a signed integer that
+%   can be represented in twos-complement form with the given number of bits,
+%   and both of these bit-representations are identical. Will throw a
+%   domain_error if any of these are out of range.
+
+
+%!  bin_swap16(+Int, -Swapped) is det.
+%!  bin_swap32(+Int, -Swapped) is det.
+%!  bin_swap64(+Int, -Swapped) is det.
+%
+%   These predicates perform unconditional byte-swapping of fixed-size integer
+%   representations. You probably don't want to use these; the bin_conv16_le/2
+%   family converts between a given endianness and this Prolog build's native
+%   format, which is usually more useful.
+%
+%   @param Int is a Prolog integer representing a fixed-size native integer. It
+%          must be ground.
+%   @param Swapped is a Prolog integer representing a byte-swapped version of Int.
+
+
+%!  bin_conv_u16_le(?Int, ?LE) is det.
+%!  bin_conv_u32_le(?Int, ?LE) is det.
+%!  bin_conv_u64_le(?Int, ?LE) is det.
+%!  bin_conv_u16_be(?Int, ?BE) is det.
+%!  bin_conv_u32_be(?Int, ?BE) is det.
+%!  bin_conv_u64_be(?Int, ?BE) is det.
+%
+%!  bin_conv_s16_le(?Int, ?LE) is det.
+%!  bin_conv_s32_le(?Int, ?LE) is det.
+%!  bin_conv_s64_le(?Int, ?LE) is det.
+%!  bin_conv_s16_be(?Int, ?BE) is det.
+%!  bin_conv_s32_be(?Int, ?BE) is det.
+%!  bin_conv_s64_be(?Int, ?BE) is det.
+%
+%   These predicates are true when Int is an integer that can be represented in
+%   the given number of bits (unsigned or signed) and LE (or BE) is the result
+%   of representing the same integer in little-endian (or big-endian) form, then
+%   reading it back as though it were a native integer of the given length and
+%   the same signedness. Will throw a domain_error if a value is outside the
+%   representable range for that length/signedness.
+%
+%   Internally, half of these are mapped to the bin_swap16/2 family (but with
+%   appropriate checks and signedness handling), while the others are mapped
+%   to (=)/2 (with the same checks).
+
+
+%!  htons(?Int, ?NetInt) is det.
+%!  htonl(?Int, ?NetInt) is det.
+%!  ntohs(?NetInt, ?Int) is det.
+%!  ntohl(?NetInt, ?Int) is det.
+%
+%   These are convenience predicates, mapped to bin_conv_u16_be/2 and
+%   bin_conv_u32_be/2.
--- binformat4pl.c
+++ binformat4pl.c
@@ -37,6 +37,181 @@
 
 #include <config.h>
 
+#ifdef WORDS_BIGENDIAN
+# define BE_NATIVE(name) name ## _native
+# define LE_NATIVE(name) name ## _swapped
+#else
+# define BE_NATIVE(name) name ## _swapped
+# define LE_NATIVE(name) name ## _native
+#endif
+
+#define TRY(call) do {if (!(call)) PL_fail;} while (0)
+#define IN_RANGE(i,sign,width,culprit) (((i) < TYPE_LO(sign,width) || (i) > TYPE_HI(sign,width)) ? PL_domain_error(#sign "int" #width, culprit) : TRUE)
+
+#define TYPE_LO(sign, width) TYPE_LO_##sign (width)
+#define TYPE_HI(sign, width) TYPE_HI_##sign (width)
+
+#define TYPE_LO_s(width) (-(TYPE_HI_s(width)) - 1)
+#define TYPE_HI_s(width) ((int64_t)((1ULL << (width-1)) - 1))
+#define TYPE_LO_u(width) (0)
+#define TYPE_HI_u(width) ((((uint64_t)TYPE_HI_s(width))<<1)+1)
+
+#define INIT_native(var) ((unsigned char*)&var);
+#define INIT_swapped(var) (((unsigned char*)&var) + sizeof(var));
+#define INCR_native(var) (*var++)
+#define INCR_swapped(var) (*--var)
+#define PL2C_integer int
+#define PL2C_uint64 uint64_t
+#define PL2C_int64 int64_t
+#define CTYP_s(bits) int##bits##_t
+#define CTYP_u(bits) uint##bits##_t
+
+
+		 /*******************************
+		 *	 DCG NONTERMINALS	*
+		 *******************************/
+
+#define BIN_INT_FUNC(signed, bits, pltyp, order) \
+static foreign_t \
+bin_##signed##int##bits##_##order(term_t i, term_t a, term_t b) \
+{ PL2C_##pltyp value; \
+  CTYP_##signed(bits) rep; \
+  unsigned char *repcodes = INIT_##order(rep); \
+  term_t list = PL_copy_term_ref(a), \
+	 head = PL_new_term_ref(); \
+  if (!PL_is_variable(i)) \
+  { TRY(PL_get_##pltyp##_ex(i, &value)); \
+    rep = value; \
+    for (int n = 0; n < sizeof(rep); n++) \
+    { TRY(PL_unify_list_ex(list, head, list) \
+	&&PL_unify_integer(head, INCR_##order(repcodes))); \
+    } \
+    return PL_unify(list, b); \
+  } else \
+  { for (int n = 0, code=0; n < sizeof(rep); n++) \
+    { TRY(PL_unify_list_ex(list, head, list) \
+	&&PL_get_integer_ex(head, &code) \
+	&&IN_RANGE(code, u, 8, head)); \
+      INCR_##order(repcodes) = (unsigned char)code; \
+    } \
+    return PL_unify_##pltyp(i, rep) \
+	&& PL_unify(list, b); \
+  } \
+}
+
+BIN_INT_FUNC(u, 8, integer, native)
+BIN_INT_FUNC(s, 8, integer, native)
+
+BIN_INT_FUNC(u, 16, integer, native)
+BIN_INT_FUNC(u, 16, integer, swapped)
+BIN_INT_FUNC(s, 16, integer, native)
+BIN_INT_FUNC(s, 16, integer, swapped)
+BIN_INT_FUNC(u, 32, uint64, native)
+BIN_INT_FUNC(u, 32, uint64, swapped)
+BIN_INT_FUNC(s, 32, int64, native)
+BIN_INT_FUNC(s, 32, int64, swapped)
+BIN_INT_FUNC(u, 64, uint64, native)
+BIN_INT_FUNC(u, 64, uint64, swapped)
+BIN_INT_FUNC(s, 64, int64, native)
+BIN_INT_FUNC(s, 64, int64, swapped)
+
+
+		 /*******************************
+		 *	 SIGN CONVERSION	*
+		 *******************************/
+
+#define BIN_CONV_SIGN_FUNC(bits, plutyp, plstyp) \
+static foreign_t \
+bin_conv_u##bits##_s##bits(term_t u, term_t s) \
+{ if (!PL_is_variable(u)) \
+  { PL2C_##plutyp value; \
+    return PL_get_##plutyp##_ex(u, &value) \
+	&& PL_unify_##plstyp(s, (CTYP_s(bits))(CTYP_u(bits))value); \
+  } else \
+  { PL2C_##plstyp value; \
+    return PL_get_##plstyp##_ex(s, &value) \
+  	&& PL_unify_##plutyp(u, (CTYP_u(bits))(CTYP_s(bits))value); \
+  } \
+}
+BIN_CONV_SIGN_FUNC(8, integer, integer)
+BIN_CONV_SIGN_FUNC(16, integer, integer)
+BIN_CONV_SIGN_FUNC(32, uint64, int64)
+BIN_CONV_SIGN_FUNC(64, uint64, int64)
+
+
+		 /*******************************
+		 *	  BYTE SWAPPING		*
+		 *******************************/
+
+#define SWAP16(value)	( ((value & (0xFF << 0)) << 8) \
+			| ((value & (0xFF << 8)) >> 8) )
+#define SWAP32(value)	( ((value & (0xFFUL << 0)) << 24)  \
+			| ((value & (0xFFUL << 8)) << 8)   \
+			| ((value & (0xFFUL << 16)) >> 8)  \
+			| ((value & (0xFFUL << 24)) >> 24) )
+#define SWAP64(value)	( ((value & (0xFFULL << 0)) << 56)  \
+			| ((value & (0xFFULL << 8)) << 40)  \
+			| ((value & (0xFFULL << 16)) << 24) \
+			| ((value & (0xFFULL << 24)) << 8)  \
+			| ((value & (0xFFULL << 32)) >> 8)  \
+			| ((value & (0xFFULL << 40)) >> 24) \
+			| ((value & (0xFFULL << 48)) >> 40) \
+			| ((value & (0xFFULL << 56)) >> 56) )
+
+#define BIN_SWAP_FUNC(bits, pltyp) \
+static foreign_t \
+bin_swap##bits(term_t native, term_t swapped) \
+{ PL2C_##pltyp value; \
+  return PL_get_##pltyp##_ex(native, &value) \
+      && PL_unify_##pltyp(swapped, SWAP##bits(value)); \
+}
+
+BIN_SWAP_FUNC(16, integer)
+BIN_SWAP_FUNC(32, int64)
+BIN_SWAP_FUNC(64, uint64)
+
+#define BIN_CONV_NATIVE_FUNC(signed, bits, pltyp) \
+static foreign_t \
+bin_conv_##signed##bits##_native(term_t a1, term_t a2) \
+{ PL2C_##pltyp value; \
+  if (!PL_is_variable(a1)) \
+    return PL_get_##pltyp##_ex(a1, &value) \
+	&& IN_RANGE(value, signed, bits, a1) \
+	&& PL_unify_##pltyp(a2, value); \
+  else \
+    return PL_get_##pltyp##_ex(a2, &value) \
+	&& IN_RANGE(value, signed, bits, a2) \
+	&& PL_unify_##pltyp(a1, value); \
+}
+
+#define BIN_CONV_SWAP_FUNC(signed, bits, pltyp) \
+static foreign_t \
+bin_conv_##signed##bits##_swapped(term_t a1, term_t a2) \
+{ PL2C_##pltyp value; \
+  if (!PL_is_variable(a1)) \
+    return PL_get_##pltyp##_ex(a1, &value) \
+	&& IN_RANGE(value, signed, bits, a1) \
+	&& PL_unify_##pltyp(a2, SWAP##bits(value)); \
+  else \
+    return PL_get_##pltyp##_ex(a2, &value) \
+	&& IN_RANGE(value, signed, bits, a2) \
+	&& PL_unify_##pltyp(a1, SWAP##bits(value)); \
+}
+
+BIN_CONV_NATIVE_FUNC(u, 16, integer)
+BIN_CONV_NATIVE_FUNC(u, 32, int64)
+BIN_CONV_NATIVE_FUNC(u, 64, uint64)
+BIN_CONV_NATIVE_FUNC(s, 16, integer)
+BIN_CONV_NATIVE_FUNC(s, 32, int64)
+BIN_CONV_NATIVE_FUNC(s, 64, int64)
+
+BIN_CONV_SWAP_FUNC(u, 16, integer)
+BIN_CONV_SWAP_FUNC(u, 32, int64)
+BIN_CONV_SWAP_FUNC(u, 64, uint64)
+BIN_CONV_SWAP_FUNC(s, 16, integer)
+BIN_CONV_SWAP_FUNC(s, 32, int64)
+BIN_CONV_SWAP_FUNC(s, 64, int64)
+
 		 /*******************************
 		 *	      REGISTER		*
 		 *******************************/
@@ -48,5 +223,47 @@
 
 install_t
 install_binformat4pl(void)
-{ 
+{ PL_register_foreign("bin_uint8", 3, bin_uint8_native, 0);
+  PL_register_foreign("bin_sint8", 3, bin_sint8_native, 0);
+  PL_register_foreign("bin_uint16", 3, bin_uint16_native, 0);
+  PL_register_foreign("bin_sint16", 3, bin_sint16_native, 0);
+  PL_register_foreign("bin_uint32", 3, bin_uint32_native, 0);
+  PL_register_foreign("bin_sint32", 3, bin_sint32_native, 0);
+  PL_register_foreign("bin_uint64", 3, bin_uint64_native, 0);
+  PL_register_foreign("bin_sint64", 3, bin_sint64_native, 0);
+  PL_register_foreign("bin_uint16_le", 3, LE_NATIVE(bin_uint16), 0);
+  PL_register_foreign("bin_sint16_le", 3, LE_NATIVE(bin_sint16), 0);
+  PL_register_foreign("bin_uint32_le", 3, LE_NATIVE(bin_uint32), 0);
+  PL_register_foreign("bin_sint32_le", 3, LE_NATIVE(bin_sint32), 0);
+  PL_register_foreign("bin_uint64_le", 3, LE_NATIVE(bin_uint64), 0);
+  PL_register_foreign("bin_sint64_le", 3, LE_NATIVE(bin_sint64), 0);
+  PL_register_foreign("bin_uint16_be", 3, BE_NATIVE(bin_uint16), 0);
+  PL_register_foreign("bin_sint16_be", 3, BE_NATIVE(bin_sint16), 0);
+  PL_register_foreign("bin_uint32_be", 3, BE_NATIVE(bin_uint32), 0);
+  PL_register_foreign("bin_sint32_be", 3, BE_NATIVE(bin_sint32), 0);
+  PL_register_foreign("bin_uint64_be", 3, BE_NATIVE(bin_uint64), 0);
+  PL_register_foreign("bin_sint64_be", 3, BE_NATIVE(bin_sint64), 0);
+  PL_register_foreign("bin_conv_u8_s8", 2, bin_conv_u8_s8, 0);
+  PL_register_foreign("bin_conv_u16_s16", 2, bin_conv_u16_s16, 0);
+  PL_register_foreign("bin_conv_u32_s32", 2, bin_conv_u32_s32, 0);
+  PL_register_foreign("bin_conv_u64_s64", 2, bin_conv_u64_s64, 0);
+  PL_register_foreign("bin_swap16", 2, bin_swap16, 0);
+  PL_register_foreign("bin_swap32", 2, bin_swap32, 0);
+  PL_register_foreign("bin_swap64", 2, bin_swap64, 0);
+  PL_register_foreign("bin_conv_u16_le", 2, LE_NATIVE(bin_conv_u16), 0);
+  PL_register_foreign("bin_conv_u32_le", 2, LE_NATIVE(bin_conv_u32), 0);
+  PL_register_foreign("bin_conv_u64_le", 2, LE_NATIVE(bin_conv_u64), 0);
+  PL_register_foreign("bin_conv_s16_le", 2, LE_NATIVE(bin_conv_s16), 0);
+  PL_register_foreign("bin_conv_s32_le", 2, LE_NATIVE(bin_conv_s32), 0);
+  PL_register_foreign("bin_conv_s64_le", 2, LE_NATIVE(bin_conv_s64), 0);
+  PL_register_foreign("bin_conv_u16_be", 2, BE_NATIVE(bin_conv_u16), 0);
+  PL_register_foreign("bin_conv_u32_be", 2, BE_NATIVE(bin_conv_u32), 0);
+  PL_register_foreign("bin_conv_u64_be", 2, BE_NATIVE(bin_conv_u64), 0);
+  PL_register_foreign("bin_conv_s16_be", 2, BE_NATIVE(bin_conv_s16), 0);
+  PL_register_foreign("bin_conv_s32_be", 2, BE_NATIVE(bin_conv_s32), 0);
+  PL_register_foreign("bin_conv_s64_be", 2, BE_NATIVE(bin_conv_s64), 0);
+  PL_register_foreign("htons", 2, BE_NATIVE(bin_conv_u16), 0);
+  PL_register_foreign("ntohs", 2, BE_NATIVE(bin_conv_u16), 0);
+  PL_register_foreign("htonl", 2, BE_NATIVE(bin_conv_u32), 0);
+  PL_register_foreign("ntohl", 2, BE_NATIVE(bin_conv_u32), 0);
 }
